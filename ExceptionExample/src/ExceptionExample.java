0d440e04da5bf63fada14c255dbef4b09c1184f4
Dave Wolf <dave45678@gmail.com>
2015-08-07 10:31:45 -0400

    First Commit

---
+++ ExceptionExample/src/ExceptionExample.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		FileInputStream fis = null;
+		int k;
+		
+		try
+		{
+			fis = new FileInputStream("c:\\myfile.txt");
+						
+			while ( (k = fis.read()) != -1)
+			{
+				System.out.println((char)k);
+			}
+		}catch (FileNotFoundException e)
+		{
+			System.out.println("You fool! There is no file!");
+		}catch (IOException e)
+		{
+			System.out.println("You fool! There is an IO exception!");
+		}
+	}
+
+}
